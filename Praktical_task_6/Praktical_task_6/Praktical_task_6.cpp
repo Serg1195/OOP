03404f9b8f3dd29fccdaa5b0f7e68def4177c677
Serg1195 <Serg.11.95@yandex.ru>
2021-11-08 23:36:19 +0700

    I can't do it

--- Praktical_task_6/Praktical_task_6/Praktical_task_6.cpp
+++ Praktical_task_6/Praktical_task_6/Praktical_task_6.cpp
@@ -1,11 +1,26 @@
 ﻿//Подгружу остальное в течении суток
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <fstream>
+#include <iosfwd>
 
+using namespace std;
 
+class stdd {
+	stdd() {	}
+	void endll()
+	{
+		std::cout << "\n\n";
 
-
+	}
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	stdd s;
+	char x='w';
+	//cin >> x;
+	cout << boolalpha << isdigit(x);
+		s::endll();//выдаёт 4 или 0 ()
 }
