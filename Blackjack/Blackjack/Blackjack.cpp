fbd2ad0d8c0359808d71d4709f382f9b54cb0dfd
Serg1195 <Serg.11.95@yandex.ru>
2021-11-14 15:48:15 +0700

    Blackjack++

--- Blackjack/Blackjack/Blackjack.cpp
+++ Blackjack/Blackjack/Blackjack.cpp
@@ -1,6 +1,7 @@
 ﻿
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -103,6 +104,58 @@ public:
     void AddltionalCards(GenericPlayer& aGenerlcPlayer);
 private:
 };
+Deck::Deck()
+{
+    m_Cards.reserve(52);
+    Populate();
+}
+Deck::~Deck()
+{}
+void Deck::Populate()
+{
+    Clear();
+    for (int s = Card::CLUBS; s <= Card::SPADES; ++s)
+    {
+        for (int r = Card::ACE; r <= Card::KING; ++r)
+        {
+            Add(new Card(static_cast<Card::rank>(r),
+                static_cast<Card::suit>(s)));
+        }
+    }
+}
+void Deck::Shuffle()
+{
+    random_shuffle(m_Cards.begin(), m_Cards.end());
+}
+void Deck::Deal(Hand& aHand)
+{
+    if (!m_Cards.empty())
+    {
+        aHand.Add(m_Cards.back());
+        m_Cards.pop_back();
+    }
+    else
+    {
+        cout << "Out of cards. Unable to deal.";
+    }
+}
+void Deck::AdditionalCards(GenericPlayer& aGenericPlayer)
+{
+    cout << endl;
+    // продолжает раздавать карты до тех пор, пока у игрока не случается
+    // перебор или пока он хочет взять еще одну карту
+    while (!(aGenericPlayer.IsBusted()) && aGenericPlayer.IsHitting())
+    {
+        Deal(aGenericPlayer);
+        cout << aGenericPlayer << endl;
+
+        if (aGenericPlayer.IsBusted())
+        {
+            aGenericPlayer.Bust();
+        }
+    }
+}
+
 
 class GenericPlayer :public Hand {
 public:
@@ -138,17 +191,102 @@ public:
 private:
 };
 
-class Game {
+class Game
+{
 public:
-    Game() {
+    Game(const vector<string>& names);
 
-    }
+    ~Game();
     void Play();
+
 private:
     Deck m_Deck;
     House m_House;
     vector<Player> m_Players;
 };
+Game::Game(const vector<string>& names)
+{
+    vector<string>::const_iterator pName;
+    for (pName = names.begin(); pName != names.end(); ++pName)
+    {
+        m_Players.push_back(Player(*pName));
+    }
+    srand(static_cast<unsigned int>(time(0)));
+    m_Deck.Populate();
+    m_Deck.Shuffle();
+}
+
+Game::~Game()
+{}
+
+void Game::Play()
+{
+    vector<Player>::iterator pPlayer;
+    for (int i = 0; i < 2; ++i)
+    {
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+        {
+            m_Deck.Deal(*pPlayer);
+        }
+        m_Deck.Deal(m_House);
+    }
+    m_House.FlipFirstCard();
+    for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+    {
+        cout << *pPlayer << endl;
+    }
+    cout << m_House << endl;
+    for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+    {
+        m_Deck.AdditionalCards(*pPlayer);
+    }
+    m_House.FlipFirstCard();
+    cout << endl << m_House;
+
+    m_Deck.AdditionalCards(m_House);
+
+    if (m_House.IsBusted())
+    {
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+        {
+            if (!(pPlayer->IsBusted()))
+            {
+                pPlayer->Win();
+            }
+        }
+    }
+    else
+    {
+        for (pPlayer = m_Players.begin(); pPlayer != m_Players.end();
+            ++pPlayer)
+        {
+            if (!(pPlayer->IsBusted()))
+            {
+                if (pPlayer->GetTotal() > m_House.GetTotal())
+                {
+                    pPlayer->Win();
+                }
+                else if (pPlayer->GetTotal() < m_House.GetTotal())
+                {
+                    pPlayer->Lose();
+                }
+                else
+                {
+                    pPlayer->Push();
+                }
+            }
+        }
+
+    }
+
+    // очищает руки всех игроков
+    for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
+    {
+        pPlayer->Clear();
+    }
+    m_House.Clear();
+}
+
 
 int main()
 {
--- Practical_task_4/Practical_task_4/Practical_task_4.cpp
+++ Practical_task_4/Practical_task_4/Practical_task_4.cpp
@@ -1,8 +1,89 @@
-﻿
+﻿#include <iostream>
+#include <cassert>
+#include <vector>
 
-#include <iostream>
+class ArrayInt
+{
+private:
+    int m_length;
+    int* m_data;
+public:
+    ArrayInt() : m_length(0), m_data(nullptr)
+    { }
+    ArrayInt(int length) : m_length(length)
+    {
+        assert(length >= 0);
+        if (length > 0)
+            m_data = new int[length];
+        else
+            m_data = nullptr;
+    }
+    ~ArrayInt()
+    {
+        delete[] m_data;
+    }
+    //читку массива и сбрасывать его длину на 0
+    void erase()
+    {
+        delete[] m_data;
+        
+            m_data = nullptr;
+        m_length = 0;
+    } 
+
+    int getLength() {
+        return m_length;
+    }
+    int& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
+    }
+    //изменять размер массива
+    void resize(int newLength)
+    {
+   
+        if (newLength == m_length)
+            return;
+        if (newLength <= 0)
+        {
+            erase();
+            return;
+        }
+        int* data = new int[newLength];           
+            if (m_length > 0)
+            {
+                int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+                for (int index = 0; index < elementsToCopy; ++index)
+                    data[index] = m_data[index];
+            }
+        delete[] m_data;            
+            m_data = data;
+        m_length = newLength;
+    }
+    //добавления нового элемента в массив
+    void insertBefore(int value, int index)
+    {
+        assert(index >= 0 && index <= m_length);
+        int* data = new int[m_length + 1];
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+        data[index] = value;
+        for (int after = index; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+        delete[] m_data;
+        m_data = data;
+        ++m_length;
+    }
+    void push_back(int value) { insertBefore(value, m_length); }
+    void popback();
+};
+void ArrayInt::popback() {
+    m_length--;
+}
 
 int main()
 {
-    std::cout <<< "Hello World!\n";
+    ArrayInt a(4);
+    std::cout << "Hello World!\n";
 }
